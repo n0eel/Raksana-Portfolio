93260d574839a535f18444b7b21783631b472738
n0el <n0elkchau@gmail.com>
2025-01-22 18:44:55 +0500

    finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import CustomCarousel from "@/components/CustomCarousel";
+import CustomCarousel from "@/components/CustomCarousel"; 
 import HeightSlider from "@/components/HeightSlider";
+import Test from "@/components/Test";
 import WeightSlider from "@/components/WeightSlider";
 import { InstagramIcon, TiktokIcon } from "@/public/icons/icons";
 import Image from "next/image";
@@ -7,38 +8,38 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="px-[150px] py-[61px]">
+    <div className="px-[30px] py-[30px] sm:px-[150px] sm:py-[61px]">
       <section className="mb-[140px]">
         <div>
           <ul className="flex items-center justify-center text-white">
             <div className="flex text-[18px] space-x-[40px]">
-              <li className="hover:border-b-[2px] border-[#FD6F00] hover:text-[19px] duration-300 cursor-pointer">Home</li>
-              <li className="hover:border-b-[2px] border-[#FD6F00] hover:text-[19px] duration-300 cursor-pointer">About Me</li>
-              <li className="hover:border-b-[2px] border-[#FD6F00] hover:text-[19px] duration-300 cursor-pointer">Experience</li>
+              <li className="text-[11px] md:hover:border-b-[2px] border-[#FD6F00] md:text-[18px] md:hover:text-[19px] duration-300 cursor-pointer">Home</li>
+              <li className="text-[11px] md:hover:border-b-[2px] border-[#FD6F00] md:text-[18px] md:hover:text-[19px] duration-300 cursor-pointer">About Me</li>
+              <li className="text-[11px] md:hover:border-b-[2px] border-[#FD6F00] md:text-[18px] md:hover:text-[19px] duration-300 cursor-pointer">Experience</li>
             </div>
           </ul>
         </div>
-        <div className="mt-[120px] flex justify-between">
-          <div className="w-[800px] mt-[100px]">
-            <p className="text-white text-[21px]">Hi I am</p>
-            <h4 className="text-[32px] text-[#FD6F00] font-semibold">Raksana Avazkhanova</h4>
-            <h1 className="text-white text-[90px] font-bold">MODEL</h1>
-            <div className="text-[21px] text-white mb-[10px]">
+        <div className="xl:mt-[120px] xl:flex xl:justify-between lg:block">
+          <div className="lg:w-[800px] mt-[100px]">
+            <p className="text-[] text-white md:text-[21px]">Hi I am</p>
+            <h4 className="md:text-[32px] text-[#FD6F00] font-semibold">Raksana Avazkhanova</h4>
+            <h1 className="text-white md:text-[90px] font-bold">MODEL</h1>
+            <div className="md:text-[21px] text-white mb-[10px]">
               <span className="text-[#FD6F00]">ENG</span>
               {/* eslint-disable-next-line react/no-unescaped-entities */}
               / I graduated Art Models and I've been working in this sphere for 7 years.
             </div>
-            <div className="text-[21px] text-white mb-[10px]">
+            <div className="md:text-[21px] text-white mb-[10px]">
               <span className="text-[#FD6F00]">RU</span>
               {/* eslint-disable-next-line react/no-unescaped-entities */}
               / Я училась в агентстве Art Models и работаю в этой сфере 7 лет.</div>
-            <div className="text-[21px] text-white mb-[10px]">
+            <div className="md:text-[21px] text-white mb-[10px]">
               <span className="text-[#FD6F00]">UZ</span>
               {/* eslint-disable-next-line react/no-unescaped-entities */}
               / Men Art Models agentligida o'qiganman va ushbu sohada 7 yildan beri ishlab kelaman.</div>
           </div>
           <div className="flex flex-col items-center">
-            <Image className="rounded-full w-[420px] h-[550px]" src={"/images/Hero.jpg"} alt="Hero Image" width={485} height={485} />
+            <Image className="w-[130px] h-[180px] rounded-full sm:w-[420px] sm:h-[550px] xl:w-[250px] xl:h-[350px]" src={"/images/Hero.jpg"} alt="Hero Image" width={485} height={485} />
             <div className="pt-[20px] flex items-center space-x-[20px]">
               <Link className="hover:scale-[1.1] duration-300 cursor-pointer" href={"https://www.instagram.com/raksana_avazhanova/"}>
                 <InstagramIcon />
@@ -49,12 +50,12 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="flex items-center mt-[70px] justify-between">
-          <div>
-            <Image className="rounded-full w-[420px] h-[550px]" src={"/images/Hero-2.jpg"} alt="Hero Image" width={485} height={485} />
+        <div className="grid items-center justify-center mt-[70px] xl:flex xl:items-center xl:justify-between lg:block">
+          <div className="flex flex-col items-center">
+            <Image className="w-[130px] h-[180px] rounded-full sm:w-[420px] sm:h-[550px] xl:w-[250px] xl:h-[350px]" src={"/images/Hero-2.jpg"} alt="Hero Image" width={485} height={485} />
           </div>
-          <div className="space-y-[30px] w-[500px]">
-            <strong className="text-[60px] text-white">About Me</strong>
+          <div className="w-[150px] lg:block lg:w-[200px] xl:space-y-[30px] xl:w-[500px] mt-[20px]">
+            <strong className="lg:mt-[60px] sm:text-[40px] xl:text-[60px] text-white">About Me</strong>
             <p></p>
             <HeightSlider />
             <WeightSlider />
@@ -63,7 +64,7 @@ export default function Home() {
       </section>
       <section>
         <div>
-          <h1 className="text-white font-bold text-[65px] text-center">Experience</h1>
+          <h1 className="text-[20px] sm:text-[40px] text-white font-bold xl:text-[65px] text-center">Experience</h1>
           <CustomCarousel />
         </div>
       </section>
--- components/CustomCarousel.tsx
+++ components/CustomCarousel.tsx
@@ -10,81 +10,81 @@ const CustomCarousel: React.FC = () => {
 
   return (
     <Carousel afterChange={onChange}>
-      <div className='mb-[50px]'>
+      <div className='mb-[50px] lg:w-[300px]'>
         <div className='text-center space-y-[20px] mt-[30px]'>
-          <strong className='text-[#FD6F00] text-[40px]'>Honor - <span className='text-white text-[35px] font-medium'>Electronics Company</span></strong>
+          <strong className='text-[#FD6F00] md:text-[40px] text-[10px]'>Honor - <span className='text-white md:text-[35px] text-[10px] font-medium'>Electronics Company</span></strong>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[600px] h-[400px]' src={"/images/honor-4.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[350px] h-[400px]' src={"/images/honor-3.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[140px] h-[100px] lg:w-[600px] lg:h-[400px] sm:w-[140px] sm:h-[100px]' src={"/images/honor-4.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[100px] h-[100px] lg:w-[350px] lg:h-[400px] sm:w-[100px] sm:h-[100px]' src={"/images/honor-3.jpg"} alt='honor image' width={300} height={350} />
           </div>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[350px] h-[400px]' src={"/images/honor-1.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[600px] h-[400px]' src={"/images/honor-2.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[90px] h-[100px] lg:w-[350px] lg:h-[400px] sm:w-[90px] sm:h-[100px]' src={"/images/honor-1.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[150px] h-[100px] lg:w-[600px] lg:h-[400px] sm:w-[150px] sm:h-[100px]' src={"/images/honor-2.jpg"} alt='honor image' width={300} height={350} />
           </div>
         </div>
       </div>
-      <div className='mb-[50px]'>
+      <div className='mb-[50px] lg:w-[300px]'>
         <div className='text-center space-y-[20px] mt-[30px]'>
-          <strong className='text-[#FD6F00] text-[40px]'>Misskamila - <span className='text-white text-[35px] font-medium'>Bridal Shop</span></strong>
+          <strong className='text-[#FD6F00] md:text-[40px] text-[10px]'>Misskamila - <span className='text-white md:text-[35px] text-[10px] font-medium'>Bridal Shop</span></strong>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[350px] h-[400px]' src={"/images/Misskamila-2.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[350px] h-[400px]' src={"/images/Misskamila-3.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[350px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Misskamila-2.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[350px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Misskamila-3.jpg"} alt='honor image' width={300} height={350} />
           </div>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[350px] h-[400px]' src={"/images/Misskamila-4.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[350px] h-[400px]' src={"/images/Misskamila-5.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[350px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Misskamila-4.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[350px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Misskamila-5.jpg"} alt='honor image' width={300} height={350} />
           </div>
         </div>
       </div>
-      <div className='mb-[50px]'>
+      <div className='mb-[50px] lg:w-[300px]'>
         <div className='text-center space-y-[20px] mt-[30px]'>
-          <strong className='text-[#FD6F00] text-[40px]'>Piplstea - <span className='text-white text-[35px] font-medium'>Bubble tea</span></strong>
+          <strong className='text-[#FD6F00] md:text-[40px] text-[10px]'>Piplstea - <span className='text-white md:text-[35px] text-[10px] font-medium'>Bubble tea</span></strong>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[320px] h-[400px]' src={"/images/Piplstea-1.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[320px] h-[400px]' src={"/images/Piplstea-3.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Piplstea-1.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Piplstea-3.jpg"} alt='honor image' width={300} height={350} />
           </div>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[320px] h-[400px]' src={"/images/Piplstea-4.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[320px] h-[400px]' src={"/images/Piplstea-5.jpg"} alt='honor image' width={300} height={350} />
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className='text-center space-y-[20px] mt-[30px]'>
-          <strong className='text-[#FD6F00] text-[40px]'>Miobeautyuz - <span className='text-white text-[35px] font-medium'>Beauty, cosmetic & personal care</span></strong>
-          <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[320px] h-[400px]' src={"/images/Miobeautyuz-1.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[320px] h-[400px]' src={"/images/Miobeautyuz-2.jpg"} alt='honor image' width={300} height={350} />
-          </div>
-          <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[320px] h-[400px]' src={"/images/Miobeautyuz-3.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[320px] h-[400px]' src={"/images/Miobeautyuz-4.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Piplstea-4.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Piplstea-5.jpg"} alt='honor image' width={300} height={350} />
           </div>
         </div>
       </div>
       <div>
-        <div className='text-center space-y-[20px] mt-[30px]'>
-          <strong className='text-[#FD6F00] text-[40px]'>Zara - <span className='text-white text-[35px] font-medium'>Clothing, accessories, beauty products and perfumes.</span></strong>
+        <div className='text-center space-y-[20px] mt-[30px] lg:w-[300px]'>
+          <strong className='text-[#FD6F00] md:text-[40px] text-[10px]'>Miobeautyuz - <span className='text-white md:text-[35px] text-[10px] font-medium'>Beauty, cosmetic & personal care</span></strong>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[280px] h-[400px]' src={"/images/zara-1.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[280px] h-[400px]' src={"/images/zara-2.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Miobeautyuz-1.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Miobeautyuz-2.jpg"} alt='honor image' width={300} height={350} />
           </div>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[280px] h-[400px]' src={"/images/zara-3.jpg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[280px] h-[400px]' src={"/images/zara-4.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Miobeautyuz-3.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/Miobeautyuz-4.jpg"} alt='honor image' width={300} height={350} />
           </div>
         </div>
       </div>
       <div>
-        <div className='text-center space-y-[20px] mt-[30px]'>
-          <strong className='text-[#FD6F00] text-[40px]'>fashiondiamond - <span className='text-white text-[35px] font-medium'>Jewelry/watches.</span></strong>
+        <div className='text-center space-y-[20px] mt-[30px] lg:w-[300px]'>
+          <strong className='text-[#FD6F00] md:text-[40px] text-[10px]'>Zara - <span className='text-white md:text-[35px] text-[10px] font-medium'>Clothing, accessories, beauty products and perfumes.</span></strong>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[320px] h-[400px]' src={"/images/fashiondiamond-1.jpeg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[320px] h-[400px]' src={"/images/fashiondiamond-2.jpeg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[80px] h-[100px] lg:w-[280px] lg:h-[400px] sm:w-[80px] sm:h-[100px]' src={"/images/zara-1.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[80px] h-[100px] lg:w-[280px] lg:h-[400px] sm:w-[80px] sm:h-[100px]' src={"/images/zara-2.jpg"} alt='honor image' width={300} height={350} />
           </div>
           <div className='flex space-x-[20px] justify-center'>
-            <Image className='w-[320px] h-[400px]' src={"/images/fashiondiamond-3.jpeg"} alt='honor image' width={300} height={350} />
-            <Image className='w-[320px] h-[400px]' src={"/images/fashiondiamond-4.jpeg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[80px] h-[100px] lg:w-[280px] lg:h-[400px] sm:w-[80px] sm:h-[100px]' src={"/images/zara-3.jpg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[80px] h-[100px] lg:w-[280px] lg:h-[400px] sm:w-[80px] sm:h-[100px]' src={"/images/zara-4.jpg"} alt='honor image' width={300} height={350} />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className='text-center space-y-[20px] mt-[30px] lg:w-[300px]'>
+          <strong className='text-[#FD6F00] md:text-[40px] text-[10px]'>fashiondiamond - <span className='text-white md:text-[35px] text-[10px] font-medium'>Jewelry/watches.</span></strong>
+          <div className='flex space-x-[20px] justify-center'>
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/fashiondiamond-1.jpeg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/fashiondiamond-2.jpeg"} alt='honor image' width={300} height={350} />
+          </div>
+          <div className='flex space-x-[20px] justify-center'>
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/fashiondiamond-3.jpeg"} alt='honor image' width={300} height={350} />
+            <Image className='w-[85px] h-[100px] lg:w-[320px] lg:h-[400px] sm:w-[85px] sm:h-[100px]' src={"/images/fashiondiamond-4.jpeg"} alt='honor image' width={300} height={350} />
           </div>
         </div>
       </div>
